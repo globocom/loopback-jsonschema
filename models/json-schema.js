1d2413e1fa5dc49056bd0cb890a3d2ec2fc5839f
Guilherme Cirne <gcirne@gmail.com>
2014-02-19 17:21:21 -0300

    [#127] Adds $schema pointing to draft-3 for every schema POSTed.

--- models/json-schema.js
+++ models/json-schema.js
@@ -6,10 +6,22 @@ var db = loopback.memory('db');
 var JsonSchema = module.exports = db.createModel('json-schema');
 
 JsonSchema.on('attached', function(app) {
+    JsonSchema.beforeSave = function(next, jsonSchema) {
+        beforeSave(jsonSchema, next)
+    };
     JsonSchema.afterSave = function(done) {
-        var JsonSchemaModel = db.createModel(this.title);
-        app.model(JsonSchemaModel);
-        loopbackExplorer(app);
-        done();
-    }
+        afterSave(this, app, done);
+    };
 });
+
+function beforeSave(jsonSchema, next) {
+    jsonSchema.$schema = "http://json-schema.org/draft-03/hyper-schema#";
+    next();
+}
+
+function afterSave(jsonSchema, app, done) {
+    var JsonSchemaModel = db.createModel(jsonSchema.title);
+    app.model(JsonSchemaModel);
+    loopbackExplorer(app);
+    done();
+};
