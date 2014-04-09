d14017b4e1887d3b7822570b21763f163bf30624
Guilherme Cirne <gcirne@gmail.com>
2014-04-09 17:17:17 -0300

    Extract JsonSchema hooks to separate module for easier testing.

--- test/unit/models/json-schema-hooks.test.js
+++ test/unit/models/json-schema-hooks.test.js
@@ -3,9 +3,9 @@ var loopback = require('loopback');
 
 var app = loopback();
 var initLoopbackJsonSchema = require('../../../index');
-var JsonSchema = require('../../../models/json-schema');
+var jsonSchemaHooks = require('../../../models/json-schema-hooks');
 
-describe('JsonSchema', function() {
+describe('jsonSchemaHooks', function() {
     var dummy = function() {};
 
     beforeEach(function() {
@@ -15,22 +15,22 @@ describe('JsonSchema', function() {
     describe('.beforeSave', function() {
         it('should set $schema to hyper-schema draft-04 by default', function() {
             var jsonSchema = {};
-            JsonSchema.beforeSave(dummy, jsonSchema);
+            jsonSchemaHooks.beforeSave(dummy, jsonSchema);
             expect(jsonSchema.$schema).to.equal('http://json-schema.org/draft-04/hyper-schema#');
         });
 
         it('should allow overriding of $schema', function() {
             var jsonSchema = {$schema: 'http://json-schema.org/draft-03/hyper-schema#'};
-            JsonSchema.beforeSave(dummy, jsonSchema);
+            jsonSchemaHooks.beforeSave(dummy, jsonSchema);
             expect(jsonSchema.$schema).to.equal('http://json-schema.org/draft-03/hyper-schema#');
         });
     });
 
     describe('.afterSave', function() {
         it('should create model defined by the json schema provided', function() {
-            JsonSchema.create({'title': 'test'}, function() {
-                expect(loopback.getModel('test')).to.exist;
-            });
+            var jsonSchema = {'title': 'test'};
+            jsonSchemaHooks.afterSave(dummy, jsonSchema, app);
+            expect(loopback.getModel('test')).to.exist;
         });
     });
 });
