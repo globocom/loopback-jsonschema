43db143a2d2044d742088950273cbc197502a472
Guilherme Cirne <gcirne@gmail.com>
2014-04-15 19:23:39 -0300

    Remove test.

--- test/integration/models/json-schema.test.js
+++ test/integration/models/json-schema.test.js
@@ -17,17 +17,6 @@ describe('JsonSchema', function() {
             });
         });
 
-        xit('should add links', function() {
-            JsonSchema.create({modelName: 'test'}, function(_, jsonSchema) {
-                var rels = jsonSchema.links.map(function(link) {
-                    return link.rel;
-                });
-                ['self', 'item', 'update', 'delete'].forEach(function(expectedRel) {
-                    expect(rels.indexOf(expectedRel)).to.be.at.least(0);
-                });
-            });
-        });
-
         it('should create model defined by the json schema provided', function() {
             JsonSchema.create({modelName: 'test'}, function() {
                 expect(loopback.getModel('test')).to.exist;
