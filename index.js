bd3cc29935c9fa891fc8e7d3fcd1db51cafb94d9
Alberto Leal <albertonb@gmail.com>
2014-04-29 15:29:49 -0300

    merge

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 module.exports = loopbackJsonSchema = {};
 
+var loopback = require('loopback');
 var JsonSchema = require('./lib/models/json-schema');
 var jsonSchemaMiddleware = require('./lib/middleware/json-schema.middleware');
 
 loopbackJsonSchema.initLoopbackJsonSchema = function(app) {
+    var db = app.dataSources.jsonSchemaDb || loopback.memory();
+    JsonSchema.attachTo(db);
+
     app.model(JsonSchema);
 
     app.on('middleware:preprocessors', function() {
