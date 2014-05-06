567fe153fc42e965d532c01ce6c8c9e7b615a52c
Alberto Leal <albertonb@gmail.com>
2014-05-06 17:18:09 -0300

    Add headers when creating a new object.

--- test/integration/middleware/http-header.middleware.test.js
+++ test/integration/middleware/http-header.middleware.test.js
@@ -13,7 +13,7 @@ app.set('restApiRoot', '/api');
 app.use(app.get('restApiRoot'), httpHeaderMiddleware());
 app.installMiddleware();
 
-describe('JsonSchema', function() {
+describe('http-header.middleware', function() {
     beforeEach(function() {
         loopbackJsonSchema.initLoopbackJsonSchema(app);
         var jsonSchema = new JsonSchema({modelName: 'test-car', collectionName: 'test-cars'});
@@ -27,30 +27,17 @@ describe('JsonSchema', function() {
         Car = loopback.getModel('test-car');
     });
 
-
-    describe('resource', function () {
-        it("should not add the header 'Link' when accessing collection list", function () {
+    it("should add the header 'Link' when accessing an specific resource", function (done) {
+        Car.create({name: 'C3', color: 'black'}, function(err, car) {
             supertest(app)
-              .get('/api/test-cars')
-              .expect(200)
-              .end(function (err, res) {
-                expect(err).to.not.exist;
-                expect(res.headers['content-type']).to.eql('application/json; charset=utf-8');
-          });
-        });
-
-        it("should add the header 'Link' when accessing an specific resource", function (done) {
-            Car.create({name: 'C3', color: 'black'}, function(err, car) {
-                supertest(app)
-                    .get('/api/test-cars/'+ car.id)
-                    .expect(200)
-                    .end(function (err, res) {
-                        expect(err).to.not.exist;
-                        expect(res.headers['link']).to.exist;
-                        expect(res.headers['content-type']).to.match(/^application\/json; profile='.*\/api\/json-schemas\/.*'/);
-                        done();
-                    });
-            });
+                .get('/api/test-cars/'+ car.id)
+                .expect(200)
+                .end(function (err, res) {
+                    expect(err).to.not.exist;
+                    expect(res.headers['link']).to.exist;
+                    expect(res.headers['content-type']).to.match(/^application\/json; profile='.*\/api\/json-schemas\/.*'/);
+                    done();
+                });
         });
     });
 });
