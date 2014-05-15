be4debccf77b4c6a7fe0a3abc3f67ffdd2343e82
Alberto Leal <albertonb@gmail.com>
2014-05-15 15:13:08 -0300

    Return 404 on GET /collection-schemas with invalid schema id.

---
+++ test/acceptance/collection-schema.test.js
@@ -0,0 +1,49 @@
+require('../support');
+
+var expect = require('chai').expect;
+var loopback = require('loopback');
+var request = require('supertest');
+
+var loopbackJsonSchema = require('../../index');
+var JsonSchema = require('../../lib/models/json-schema');
+var jsonSchemaMiddleware = require('../../lib/middleware/json-schema.middleware');
+
+var app = loopback();
+app.set('restApiRoot', '/api');
+
+describe('collection-schema', function() {
+    beforeEach(function() {
+        loopbackJsonSchema.initLoopbackJsonSchema(app);
+    });
+
+    describe('GET /collection-schemas/:schemaId', function () {
+       describe('when ItemSchema exists', function () {
+            xit('should return Collection schema', function (done) {
+                request(app)
+                    .get('/api/collection-schemas/my-schema-id')
+                    .expect(200)
+                    .end(function (err, res) {
+                        if (err) { throw err };
+
+                        expect(res.body).to.not.be.emtpy;
+                        expect(res.body).to.include.keys(['$schema', 'title', 'type', 'properties']);
+                        done();
+                    });
+            });
+        });
+
+       describe('when ItemSchema does not exist', function () {
+            it('should return 404', function (done) {
+                request(app)
+                    .get('/api/collection-schemas/invalid-schema-id')
+                    .expect(404)
+                    .end(function (err, res) {
+                        if (err) { throw err };
+
+                        expect(res.body).to.not.be.emtpy;
+                        done();
+                    });
+            });
+        });
+    });
+});
