f4c9549bdd383daf155c92e5dae3037ad9f299e5
Guilherme Cirne <gcirne@gmail.com>
2014-05-19 17:06:31 -0300

    Rename collection schema tests.

--- test/acceptance/get-collection-schema.test.js
+++ test/acceptance/get-collection-schema.test.js
@@ -14,51 +14,47 @@ app.use(app.get('restApiRoot'), jsonSchemaMiddleware());
 loopbackJsonSchema.init(app);
 app.installMiddleware();
 
-describe('collection-schema', function() {
-    describe('GET /collection-schemas/:schemaId', function () {
-       describe('when ItemSchema exists', function () {
-            var jsonSchemaId;
+describe('GET /collection-schemas/:schemaId', function () {
+   describe('when item schema exists', function () {
+        var jsonSchemaId;
 
-            beforeEach(function (done) {
-                JsonSchema.create({
-                    modelName: 'person',
-                    collectionName: 'people',
-                    title: 'Person',
-                    collectionTitle: 'People',
-                    type: 'object',
-                    properties: {}
-                }, function(err, jsonSchema) {
-                    if (err) { throw err };
-                    jsonSchemaId = jsonSchema.id;
-                    done();
-                });
-            });
-
-            it('should return Collection schema', function (done) {
-                request(app)
-                    .get('/api/collection-schemas/' + jsonSchemaId)
-                    .expect(200)
-                    .end(function (err, res) {
-                        if (err) { throw err };
-
-                        expect(res.body).to.not.be.empty;
-                        expect(res.body).to.include.keys(['$schema', 'title', 'type', 'items']);
-                        done();
-                    });
+        beforeEach(function (done) {
+            JsonSchema.create({
+                modelName: 'person',
+                collectionName: 'people',
+                title: 'Person',
+                collectionTitle: 'People',
+                type: 'object',
+                properties: {}
+            }, function(err, jsonSchema) {
+                if (err) { throw err };
+                jsonSchemaId = jsonSchema.id;
+                done();
             });
         });
 
-       describe('when ItemSchema does not exist', function () {
-            it('should return 404', function (done) {
-                request(app)
-                    .get('/api/collection-schemas/invalid-schema-id')
-                    .expect(404)
-                    .end(function (err, res) {
-                        if (err) { throw err };
+        it('should return Collection schema', function (done) {
+            request(app)
+                .get('/api/collection-schemas/' + jsonSchemaId)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) { throw err };
+                    expect(res.body).to.not.be.empty;
+                    expect(res.body).to.include.keys(['$schema', 'title', 'type', 'items']);
+                    done();
+            });
+        });
+    });
 
-                        expect(res.body).to.not.be.emtpy;
-                        done();
-                    });
+   describe('when item schema does not exist', function () {
+        it('should return 404', function (done) {
+            request(app)
+                .get('/api/collection-schemas/invalid-schema-id')
+                .expect(404)
+                .end(function (err, res) {
+                    if (err) { throw err };
+                    expect(res.body).to.not.be.emtpy;
+                    done();
             });
         });
     });
