1df172be730c58f45f9e2649530e9537357093fc
Guilherme Cirne <gcirne@gmail.com>
2014-05-22 17:43:45 -0300

    Acceptance test for CORS headers on OPTIONS request to /collection-schemas/:id.

--- test/acceptance/options-collection-schema.test.js
+++ test/acceptance/options-collection-schema.test.js
@@ -1,7 +1,33 @@
 require('../support');
 
-describe('OPTION /collection-schemas/:id', function() {
-    xit('pending', function() {
+var expect = require('chai').expect;
+var loopback = require('loopback');
+var request = require('supertest');
 
+var loopbackJsonSchema = require('../../index');
+
+var app = loopback();
+app.set('restApiRoot', '/api');
+app.installMiddleware();
+
+describe('OPTION /collection-schemas/:id', function() {
+    var response;
+
+    before(function(done) {
+        request(app)
+            .options('/api/collection-schemas/1')
+            .set('Access-Control-Request-Headers', 'Content-Type')
+            .expect(204)
+            .end(function (err, res) {
+                if (err) { throw err };
+                response = res;
+                done();
+        });
+    });
+
+    it('should include CORS headers', function() {
+        expect(response.headers['access-control-allow-origin']).to.eq('*');
+        expect(response.headers['access-control-allow-methods']).to.eq('GET,HEAD,PUT,PATCH,POST,DELETE');
+        expect(response.headers['access-control-allow-headers']).to.eq('Content-Type');
     });
 });
