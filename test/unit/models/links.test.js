f49f399b7f67c0f0e72da0655a7d4d7ed6256eaa
Guilherme Cirne <gcirne@gmail.com>
2014-05-23 17:04:02 -0300

    Links no longer depends on LJSRequest.

--- test/unit/models/links.test.js
+++ test/unit/models/links.test.js
@@ -14,36 +14,33 @@ describe('Links', function() {
         var allLinks;
 
         beforeEach(function() {
-            var req = null;
-            var ljsReq = new LJSRequest(req, app);
-            this.sinon.stub(ljsReq, 'schemeAndAuthority').returns('http://example.org');
             var defaultLinks = [
-                { rel: 'self', href: 'http://example.org/api' }
+                { rel: 'self', href: '/api' }
             ];
             var customLinks = [
                 { rel: 'custom-absolute', href: 'http://other.example.org/custom-absolute' },
                 { rel: 'custom-relative', href: '/custom-relative' },
                 { rel: 'self', href: 'http://example.org/api/override/self' }
             ];
-            var links = new Links(ljsReq, defaultLinks, customLinks);
+            var links = new Links(defaultLinks, customLinks);
             allLinks = links.all();
         });
 
         it('should include default links', function() {
-            expect(allLinks[0]).to.eql({ rel: 'self', href: 'http://example.org/api' });
+            expect(allLinks[0]).to.eql({ rel: 'self', href: '/api' });
         });
 
         it('should include custom absolute links', function() {
             expect(allLinks[1]).to.eql({ rel: 'custom-absolute', href: 'http://other.example.org/custom-absolute' });
         });
 
-        it('should include custom relative links with base url added', function() {
-            expect(allLinks[2]).to.eql({ rel: 'custom-relative', href: 'http://example.org/api/custom-relative' });
+        it('should include custom relative links', function() {
+            expect(allLinks[2]).to.eql({ rel: 'custom-relative', href: '/custom-relative' });
         });
 
         it('should not allow overriding default links', function() {
             expect(allLinks).to.have.length(3);
-            expect(allLinks[0]).to.eql({ rel: 'self', href: 'http://example.org/api' });
+            expect(allLinks[0]).to.eql({ rel: 'self', href: '/api' });
         });
     });
 });
