4101f6b098a3c5d92b9fde01ed9e6d29327ac607
Guilherme Cirne <gcirne@gmail.com>
2014-06-10 16:26:13 -0300

    [#9] Acceptance test POSTing item with unsupported Content-Type.

--- test/acceptance/post-item.test.js
+++ test/acceptance/post-item.test.js
@@ -17,38 +17,65 @@ app.installMiddleware();
 describe('POST /:collection', function() {
     var itemResponse, jsonSchemaId, schemeAndAuthority;
 
-    before(function (done) {
-        ItemSchema.create({
-            modelName: 'person',
-            collectionName: 'people',
-            title: 'Person',
-            collectionTitle: 'People',
-            type: 'object',
-            properties: {}
-        }, function(err, jsonSchema) {
-            if (err) { throw err };
-            jsonSchemaId = jsonSchema.id;
-            done();
+    describe('successfully', function() {
+        before(function (done) {
+            ItemSchema.create({
+                modelName: 'person',
+                collectionName: 'people',
+                title: 'Person',
+                collectionTitle: 'People',
+                type: 'object',
+                properties: {}
+            }, function(err, jsonSchema) {
+                if (err) { throw err };
+                jsonSchemaId = jsonSchema.id;
+                done();
+            });
+        });
+
+        before(function(done) {
+            request(app)
+                .post('/api/people')
+                .set('Content-Type', 'application/json')
+                .send('{"name": "Alice"}')
+                .end(function (err, res) {
+                    if (err) { throw err };
+                    schemeAndAuthority = 'http://' + res.req._headers.host;
+                    itemResponse = res;
+                    done();
+                });
+        });
+
+        it('should return 200', function() {
+            expect(itemResponse.status).to.eq(200);
+        });
+
+        it('should correlate the item with its schema', function() {
+            var itemSchemaUrl = schemeAndAuthority + '/api/item-schemas/' + jsonSchemaId;
+            expect(itemResponse.headers['link']).to.eq('<' + itemSchemaUrl + '>; rel=describedby');
+            expect(itemResponse.headers['content-type']).to.eq('application/json; charset=utf-8; profile=' + itemSchemaUrl);
         });
     });
 
-    before(function(done) {
-        request(app)
-            .post('/api/people')
-            .set('Content-Type', 'application/json')
-            .send('{"name": "Alice"}')
-            .expect(200)
-            .end(function (err, res) {
-                if (err) { throw err };
-                schemeAndAuthority = 'http://' + res.req._headers.host;
-                itemResponse = res;
-                done();
-            });
-    });
+    describe('with unsupported Content-Type', function() {
+        before(function(done) {
+            request(app)
+                .post('/api/people')
+                .set('Content-Type', 'text/plain')
+                .send('{"name": "Alice"}')
+                .end(function (err, res) {
+                    if (err) { throw err };
+                    itemResponse = res;
+                    done();
+                });
+        });
 
-    it('should correlate the item with its schema', function() {
-        var itemSchemaUrl = schemeAndAuthority + '/api/item-schemas/' + jsonSchemaId;
-        expect(itemResponse.headers['link']).to.eq('<' + itemSchemaUrl + '>; rel=describedby');
-        expect(itemResponse.headers['content-type']).to.eq('application/json; charset=utf-8; profile=' + itemSchemaUrl);
+        it('should return 400', function() {
+            expect(itemResponse.status).to.eq(400);
+        });
+
+        it('should return error message', function() {
+            expect(itemResponse.text).to.eq('Unsupported Content-Type: <text/plain>.')
+        });
     });
 });
