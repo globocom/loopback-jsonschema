59722910d017491a68e96db81ec0821f492e72c6
Guilherme Cirne <gcirne@gmail.com>
2014-06-13 13:52:34 -0300

    [#7] Fix "items": { $ref: ... } not being absolute.

--- test/unit/http/schema-body-url-rewriter.test.js
+++ test/unit/http/schema-body-url-rewriter.test.js
@@ -93,16 +93,40 @@ describe('schemaBodyUrlRewriter', function() {
             });
         });
 
-        describe('when links and properties do not exist', function() {
+        describe('when items $ref is relative', function() {
+            beforeEach(function() {
+                originalBody.items = { $ref: '/relative' };
+                body = schemaBodyUrlRewriter.makeAbsolute(ljsReq, originalBody);
+            });
+
+            it('should make schema.$ref absolute', function() {
+                expect(body.items.$ref).to.eq('http://example.org/relative');
+            });
+        });
+
+        describe('when items $ref is absolute', function() {
+            beforeEach(function() {
+                originalBody.items = { $ref: 'http://example.org/absolute' };
+                body = schemaBodyUrlRewriter.makeAbsolute(ljsReq, originalBody);
+            });
+
+            it('should do nothing', function() {
+                expect(body.items.$ref).to.eq('http://example.org/absolute');
+            });
+        });
+
+        describe('when links, properties and items do not exist', function() {
             beforeEach(function() {
                 originalBody.links = undefined;
                 originalBody.properties = undefined;
+                originalBody.items = undefined;
                 body = schemaBodyUrlRewriter.makeAbsolute(ljsReq, originalBody);
             });
 
             it('should do nothing', function() {
                 expect(body.links).to.be.undefined;
                 expect(body.properties).to.be.undefined;
+                expect(body.items).to.be.undefined;
             });
         });
 
@@ -110,12 +134,14 @@ describe('schemaBodyUrlRewriter', function() {
             beforeEach(function() {
                 originalBody.links = [{}];
                 originalBody.properties = {};
+                originalBody.items = {};
                 body = schemaBodyUrlRewriter.makeAbsolute(ljsReq, originalBody);
             });
 
             it('should do nothing', function() {
                 expect(body.links[0]).to.eql({});
                 expect(body.properties).to.eql({});
+                expect(body.items).to.eql({});
             });
         });
 
@@ -124,24 +150,28 @@ describe('schemaBodyUrlRewriter', function() {
                 originalBody.properties = {
                     href: '/relative'
                 };
-                body = schemaBodyUrlRewriter.makeAbsolute(ljsReq, originalBody);
-            });
-
-            it('should do nothing', function() {
-                expect(body.properties.href).to.eq('/relative');
-            });
-        });
-
-        describe('when $ref is a child of something other than links or properties', function() {
-            beforeEach(function() {
-                originalBody.something = {
+                originalBody.items = {
                     href: '/relative'
                 };
                 body = schemaBodyUrlRewriter.makeAbsolute(ljsReq, originalBody);
             });
 
             it('should do nothing', function() {
-                expect(body.something.href).to.eq('/relative');
+                expect(body.properties.href).to.eq('/relative');
+                expect(body.items.href).to.eq('/relative');
+            });
+        });
+
+        describe('when $ref is a child of something other than links, properties or items', function() {
+            beforeEach(function() {
+                originalBody.something = {
+                    $ref: '/relative'
+                };
+                body = schemaBodyUrlRewriter.makeAbsolute(ljsReq, originalBody);
+            });
+
+            it('should do nothing', function() {
+                expect(body.something.$ref).to.eq('/relative');
             });
         });
     });
