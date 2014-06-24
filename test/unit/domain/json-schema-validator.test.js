7d065282abe3c80ff08b3934da1764dc00af1fe0
Alberto Leal <albertonb@gmail.com>
2014-06-24 16:33:24 -0300

    Add Draft3 support to JsonSchemaValidator class.

--- test/unit/domain/json-schema-validator.test.js
+++ test/unit/domain/json-schema-validator.test.js
@@ -13,6 +13,63 @@ describe('JsonSchemaValidator', function() {
         expect(jsonSchemaValidator).to.be.null;
     });
 
+    describe('draft3', function() {
+        beforeEach(function() {
+            jsonSchemaValidator = JsonSchemaValidator('http://json-schema.org/draft-03/hyper-schema#');
+        });
+
+        it('should be able to validate a schema following draft4 rules', function () {
+            expect(jsonSchemaValidator).to.not.be.null;
+        });
+
+        describe('#validate', function() {
+            var schema;
+
+            beforeEach(function () {
+                schema = {
+                    "title": "Example Schema",
+                    "type": "object",
+                    "properties": {
+                        "firstName": {
+                            "type": "string",
+                            "required": true
+                        },
+                        "age": {
+                            "type": "integer",
+                            "required": true
+                        }
+                    }
+                };
+            });
+
+            it('should return the formatted errors and the count', function() {
+                var errors = jsonSchemaValidator.validate({}, {});
+                expect(errors).to.have.keys(['items', 'itemCount']);
+            });
+
+            describe('required fields', function() {
+                it('should return a list of fields with error', function () {
+                    var data = {};
+
+                    var errors = jsonSchemaValidator.validate(schema, data);
+                    expect(errors.itemCount).to.eq(2);
+                    expect(errors.items).to.eql([{
+                            code: 302,
+                            message: "Missing required property",
+                            dataPath: '/firstName',
+                            schemaPath: '/properties/firstName'
+                        },
+                        {
+                            code: 302,
+                            message: "Missing required property",
+                            dataPath: '/age',
+                            schemaPath: '/properties/age'
+                    }]);
+                });
+            });
+        });
+    });
+
     describe('draft4', function() {
         beforeEach(function() {
             jsonSchemaValidator = JsonSchemaValidator('http://json-schema.org/draft-04/hyper-schema#');
@@ -53,16 +110,17 @@ describe('JsonSchemaValidator', function() {
                     var errors = jsonSchemaValidator.validate(schema, data);
                     expect(errors.itemCount).to.eq(2);
                     expect(errors.items).to.eql([{
+                            code: 302,
                             message: "Missing required property: firstName",
                             dataPath: '',
                             schemaPath: '/required/0'
                         },
                         {
+                            code: 302,
                             message: "Missing required property: age",
                             dataPath: '',
                             schemaPath: '/required/1'
-                        }
-                    ]);
+                    }]);
                 });
             });
 
@@ -81,9 +139,10 @@ describe('JsonSchemaValidator', function() {
                     var errors = jsonSchemaValidator.validate(schema, data);
                     expect(errors.itemCount).to.eq(1);
                     expect(errors.items).to.eql([{
-                      "dataPath": "/tags",
-                      "message": "Array is too short (0), minimum 1",
-                      "schemaPath": "/properties/tags/minItems"
+                        code: 400,
+                        dataPath: "/tags",
+                        message: "Array is too short (0), minimum 1",
+                        schemaPath: "/properties/tags/minItems"
                     }]);
                 });
             });
