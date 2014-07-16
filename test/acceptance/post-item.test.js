1c52e02642dce7c42c6f6d55c4bf7decf59d1b4a
Guilherme Cirne <gcirne@gmail.com>
2014-07-16 14:02:48 -0300

    Acceptance test for POST item with validation errors.

--- test/acceptance/post-item.test.js
+++ test/acceptance/post-item.test.js
@@ -72,4 +72,69 @@ describe('POST /:collection', function() {
             expect(itemResponse.body.error.message).to.eq('Unsupported Content-Type: <text/plain>.')
         });
     });
+
+    describe('with validation errors', function() {
+        before(function(done) {
+            ItemSchema.create({
+                modelName: 'person',
+                collectionName: 'people',
+                title: 'Person',
+                collectionTitle: 'People',
+                type: 'object',
+                properties: {
+                    name: {
+                        type: 'string'
+                    }
+                },
+                required: ['name']
+            }, function(err, jsonSchema) {
+                if (err) { throw err };
+                jsonSchemaId = jsonSchema.id;
+                done();
+            });
+        });
+
+        before(function(done) {
+            request(app)
+                .post('/api/people')
+                .set('Content-Type', 'application/json')
+                .send('{}')
+                .end(function (err, res) {
+                    if (err) { throw err };
+                    itemResponse = res;
+                    done();
+                });
+        });
+
+        it('should return 422 status code', function() {
+            expect(itemResponse.status).to.eq(422);
+        });
+
+        it('should return errors in body', function() {
+            var error = itemResponse.body.error;
+            expect(error.details).to.eql({
+                codes: {
+                    '/name': [
+                        302
+                    ],
+                    '_all': [
+                        'custom'
+                    ]
+                },
+                context: 'person',
+                messages: {
+                    '/name': [
+                        'Missing required property: name'
+                    ],
+                    '_all': [
+                        'Instance is invalid'
+                    ]
+                }
+            });
+            expect(error.message).to.eq('The `person` instance is not valid. Details: `_all` Instance is invalid; `/name` Missing required property: name.');
+            expect(error.name).to.eq('ValidationError');
+            expect(error.status).to.eq(422);
+            expect(error.statusCode).to.eq(422);
+        });
+    });
 });
