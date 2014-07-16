152e1410f1b218edf9da8309cba8ac6f32624699
Guilherme Cirne <gcirne@gmail.com>
2014-07-16 17:29:21 -0300

    Form-Style Query Continuation URI Templates in rels previous, next and order.

--- test/acceptance/get-collection-schema.test.js
+++ test/acceptance/get-collection-schema.test.js
@@ -89,15 +89,15 @@ describe('GET /collection-schemas/:id', function () {
                 },
                 {
                     rel: 'previous',
-                    href: schemeAndAuthority + '/api/people?filter[limit]={limit}&filter[offset]={previousOffset}&{+paginateQs}'
+                    href: schemeAndAuthority + '/api/people?filter[limit]={limit}&filter[offset]={previousOffset}{&paginateQs*}'
                 },
                 {
                     rel: 'next',
-                    href: schemeAndAuthority + '/api/people?filter[limit]={limit}&filter[offset]={nextOffset}&{+paginateQs}'
+                    href: schemeAndAuthority + '/api/people?filter[limit]={limit}&filter[offset]={nextOffset}{&paginateQs*}'
                 },
                 {
                     rel: 'order',
-                    href: schemeAndAuthority + '/api/people?filter[order]={orderAttribute} {orderDirection}&{+orderQs}'
+                    href: schemeAndAuthority + '/api/people?filter[order]={orderAttribute} {orderDirection}{&orderQs*}'
                 },
                 {
                     rel: 'custom',
