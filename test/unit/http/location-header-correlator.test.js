109f5d8f30f34b50a0c8f40c7fdfcce7f8ab3450
Wilson Júnior <wilsonpjunior@gmail.com>
2015-03-06 15:23:51 -0300

    Location-header-correlator now handling urls that ending with `/`
    Signed-off-by: Wilson Júnior <wilsonpjunior@gmail.com>

--- test/unit/http/location-header-correlator.test.js
+++ test/unit/http/location-header-correlator.test.js
@@ -10,37 +10,70 @@ describe('locationHeaderCorrelator', function() {
     var ctx, fullUrl;
 
     describe('when result was a default id', function(){
-        before(function() {
-            fullUrl = 'http://api.example.org/api/test';
-            this.sinon.stub(LJSRequest.prototype, 'fullUrl').returns(fullUrl);
-
-            ctx = {
-                req: {
-                    app: null
-                },
-                res: {
-                    set: this.sinon.stub(),
-                    status: this.sinon.stub()
-                },
-                result: {
-                    id: '123',
-                    constructor: {
-                        getIdName: function() {
-                            return 'id';
+        describe('when url not end switch /', function(){
+            beforeEach(function() {
+                fullUrl = 'http://api.example.org/api/test';
+                this.sinon.stub(LJSRequest.prototype, 'fullUrl').returns(fullUrl);
+                ctx = {
+                    req: {
+                        app: null
+                    },
+                    res: {
+                        set: this.sinon.stub(),
+                        status: this.sinon.stub()
+                    },
+                    result: {
+                        id: '123',
+                        constructor: {
+                            getIdName: function() {
+                                return 'id';
+                            }
                         }
                     }
-                }
-            };
+                };
+                locationHeaderCorrelator(ctx);
+            });
 
-            locationHeaderCorrelator(ctx);
+            it('should correlate `Location` header', function(){
+                expect(ctx.res.set).to.have.been.calledWith('Location', 'http://api.example.org/api/test/123');
+            });
+
+            it('should change response status to 201', function(){
+                expect(ctx.res.status).to.have.been.calledWith(201);
+            });
         });
 
-        it('should correlate `Location` header', function(){
-            expect(ctx.res.set).to.have.been.calledWith('Location', 'http://api.example.org/api/test/123');
-        });
+        describe('when url end switch /', function(){
+            before(function() {
+                fullUrl = 'http://api.example.org/api/test/';
+                this.sinon.stub(LJSRequest.prototype, 'fullUrl').returns(fullUrl);
+                ctx = {
+                    req: {
+                        app: null
+                    },
+                    res: {
+                        set: this.sinon.stub(),
+                        status: this.sinon.stub()
+                    },
+                    result: {
+                        id: '123',
+                        constructor: {
+                            getIdName: function() {
+                                return 'id';
+                            }
+                        }
+                    }
+                };
+                locationHeaderCorrelator(ctx);
+            });
 
-        it('should change response status to 201', function(){
-            expect(ctx.res.status).to.have.been.calledWith(201);
+            it('should correlate `Location` header', function(){
+                expect(ctx.res.set).to.have.been.calledWith('Location', 'http://api.example.org/api/test/123');
+            });
+
+            it('should change response status to 201', function(){
+                expect(ctx.res.status).to.have.been.calledWith(201);
+            });
         });
     });
 
